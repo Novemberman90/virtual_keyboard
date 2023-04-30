5008b32d92d020153e48b0ad5c74a4f9b7419868
Alexey <alexledovit@gmail.com>
2023-05-01 01:53:39 +0300

    feat: add const with keyCode (test)
    Нашел первый способ выведения клавиатуры
    В cont получил значения полученные при помощи document.onkeydown = function(event)
    Такой способ работает

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
-const keyboard = ['KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 
-'BracketRight', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 
-'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash'];
+const keyboard =[81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191];
 
 /*document.onkeydown = function(event) {
-    keyboard.push(event.code);
+    keyboard.push(event.keyCode);
     console.log(keyboard);
 }*/
 
-function init () {
-    let out = '';
-    for (let i = 0; i < keyboard.length; i++ ) {
-        out += '<div class = "key-btn">'+String.fromCharCode(keyboard[i])+'</div>'
+function init(){
+    let out = '' ;
+    for ( let i = 0; i < keyboard.length; i++ ) {
+        out += '<div class = "key-btn">' + String.fromCharCode(keyboard[i]) + '</div>';
     }
     document.querySelector('#keyboard').innerHTML = out;
 }
+
 init();
