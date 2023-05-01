e440f664157c909cacd995f048dfa8418371fa0d
Alexey <alexledovit@gmail.com>
2023-05-02 02:56:06 +0300

    feat: пытался отобразить нажатие кнопки в textarea
    Проси кто это будет читать. ))
    Я это сделал за вечер в спешке абсолютно с сырыми знаниями...

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ function init(){
         if (i == 12 || i == 23){
             out += '<div class="clearfix"></div>'
         }
-        out += '<div class = "key-btn">' + (keyboard[i]) + '</div>';
+        out += '<div class = "key_btn">' + (keyboard[i]) + '</div>';
     }
     
     document.querySelector('.keyboard').innerHTML = out;
@@ -24,20 +24,28 @@ init();
 document.onkeydown = function(event){
     console.log(event.code);
     console.log(event.key);
-    document.querySelectorAll('.keyboard .key-btn').forEach(function(element){
+    document.querySelectorAll('.keyboard .key_btn').forEach(function(element){
         element.classList.remove('_active');
     });
     
-   document.querySelector('.keyboard .key-btn[data=" '+ event.key +' "]').classList.add('_active');
+  // document.querySelector('.keyboard .key_btn[data=" '+ event.code +' "]').classList.add('_active');
 }
 
-document.querySelectorAll('.keyboard .key-btn').forEach(function(element){
+document.querySelectorAll('.keyboard .key_btn').forEach(function(element){
     element.onclick = function(event){
-        document.querySelectorAll('.keyboard .key-btn').forEach(function(element){
+        document.querySelectorAll('.keyboard .key_btn').forEach(function(element){
             element.classList.remove('_active');
         });
         let code = this.getAttribute('data');
         this.classList.add('_active');
         console.log(code);
     }
-})
+});
+
+const buttons = document.querySelectorAll('.key_btn');
+const textArea = document.querySelectorAll('#textarea');
+buttons.forEach(key_btn => {
+    key_btn.addEventListener('click', () => {
+        textArea.value += key_btn.innerText
+    });
+});
